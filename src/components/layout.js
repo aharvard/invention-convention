df88ecf88f66005399b969ac474201961117c59f
Andrew Harvard <andrew.t.harvard@gmail.com>
2019-02-19 23:47:33 -0600

    remove header

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { StaticQuery, graphql } from "gatsby";
 
 import "./remedy.scss";
 
-import Header from "./header";
+// import Header from "./header";
 // import Footer from "./footer";
 
 const Layout = ({ children }) => (
@@ -20,7 +20,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
 
         <main>{children}</main>
 
