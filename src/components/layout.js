a276546730e15f378376c321fd8cc4a04d8a1fc3
Andrew Harvard <andrew.t.harvard@gmail.com>
2019-02-20 00:03:59 -0600

    updating file names

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,8 @@ import { StaticQuery, graphql } from "gatsby";
 
 import "./remedy.scss";
 
-import Header from "./header.scss";
-import Footer from "./footer.scss";
+import Header from "./header";
+import Footer from "./footer";
 
 const Layout = ({ children }) => (
   <StaticQuery
