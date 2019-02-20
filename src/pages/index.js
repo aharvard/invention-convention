f10ee08ab07159c6e8637c02b16ed5ead55cad00
Andrew Harvard <andrew.t.harvard@gmail.com>
2019-02-20 14:50:48 -0600

    index update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-import Layout from "../components/layout";
-// import Image from "../components/Image";
 import SEO from "../components/seo";
-import { Form } from "../components/form";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <Form />
-    {/* <Image /> */}
-  </Layout>
+  <>
+    <SEO title="Home" keywords={[`stem`, `upper cumberland`, `cookeville`]} />
+    <h1>Invention Convention</h1>
+    <p>2019 Website Coming Soon!</p>
+  </>
 );
 
 export default IndexPage;
