4af104b2a60b76a29b00572d5f82f3900a62d8d8
Andrew Harvard <andrew.t.harvard@gmail.com>
2019-02-19 23:56:35 -0600

    bring back header and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,8 @@ import { StaticQuery, graphql } from "gatsby";
 
 import "./remedy.scss";
 
-// import Header from "./header";
-// import Footer from "./footer";
+import Header from "./header";
+import Footer from "./footer";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -20,11 +20,11 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+        <Header siteTitle={data.site.siteMetadata.title} />
 
         <main>{children}</main>
 
-        {/* <Footer /> */}
+        <Footer />
       </>
     )}
   />
